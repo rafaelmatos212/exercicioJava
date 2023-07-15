3ae9446aeb12a8247f8162b428f9663007f89c51
Rafael Matos <92494823+rafaelmatos212@users.noreply.github.com>
2023-07-15 14:49:20 -0300

    Add files via upload

---
+++ Atividade 4/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o nome do vendedor: ");
+        String nomeVendedor = scan.next();
+
+        System.out.println("Digite o salário do vendedor: ");
+        double salarioFixo = (double)scan.nextFloat();
+
+        System.out.println("Total de vendas efetuada no mês: ");
+        double totalVendas = (double)scan.nextFloat();
+
+        double salarioFinal = totalVendas * 0.15 + salarioFixo;
+
+        System.out.println("Vendedor: " + nomeVendedor + "\nSalario: " + salarioFixo + "\nSalário final: " + salarioFinal);
+    }
+}
