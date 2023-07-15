ffe003f5bc771f9e0e8b30f5355aeecda49db990
Rafael Matos <rafaelmatos212@gmail.com>
2023-07-15 14:57:57 -0300

    Acrescentado comentário sobre a atividade

--- Atividade 4/src/Main.java
+++ Atividade 4/src/Main.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
+
+    /*
+    Escreva um algoritmo que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas
+    por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas
+    efetuadas, informar o seu nome, o salário fixo e salário no final do mês;
+    */
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
