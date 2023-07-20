a62f54c3c384feecbbb041b2b037b1d5c6cd8c35
Rafael Matos <rafaelmatos212@gmail.com>
2023-07-19 23:36:22 -0300

    Adicionado atividade 26 ao repositório

---
+++ Atividade 26/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+    Faça um algoritmo que leia um número de 1 a 5 e escreva por extenso. Caso o usuário digite um número
+    que não esteja neste intervalo, exibir a seguinte mensagem: número inválido.
+    */
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 5: ");
+        int numero = scan.nextInt();
+
+        switch(numero){
+            case 1:
+                System.out.println("Um");
+                break;
+
+            case 2:
+                System.out.println("Dois");
+                break;
+
+            case 3:
+                System.out.println("Três");
+                break;
+
+            case 4:
+                System.out.println("Quatro");
+                break;
+
+            case 5:
+                System.out.println("Cinco");
+                break;
+
+            default:
+                System.out.println("Número inválido");
+                break;
+
+        }
+
+    }
+}
